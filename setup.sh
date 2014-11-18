824d99100251949dd7e838baa5a7b722087b8f65
Jake Askeland, Robert Bosch LLC <jake.askeland@us.bosch.com>
2014-11-17 16:05:25 -0800

    adds repo clone step for pjfa

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install mercurial vim-gtk cscope exuberant-ctags libevent-dev
+sudo apt-get install mercurial vim-gtk cscope exuberant-ctags libevent-dev chromium-browser watchman
 tmux_version=$(tmux -V)
 if [[ $? != 0 || "${tmux_version}" != "tmux 1.8" ]]; then
     gunzip -c tmux_1.8.orig.tar.gz | tar xvf -
@@ -12,3 +12,10 @@ if [[ $? != 0 || "${tmux_version}" != "tmux 1.8" ]]; then
     rm -r tmux-1.8
 fi
 cp -R -i .astylerc .bashrc bin .gdbinit .gitconfig hg-prompt .hgrc hgwatchman .rviz .vim .vimrc ~
+cd ~
+mkdir -p workspace
+cd workspace
+if [[ ! -d pjfa ]]; then
+    ssh-copy-id pjfa.pal.us.bosch.com
+    hg clone ssh://asj1pal@pjfa.pal.us.bosch.com///repos/pjfa
+fi
