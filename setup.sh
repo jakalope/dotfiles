adc2ec7976cb8816e2f72429c4ad628793a42ecb
Jake Askeland, Robert Bosch LLC <jake.askeland@us.bosch.com>
2014-11-24 09:52:13 -0800

    adds watchman cleanup in setup

--- setup.sh
+++ setup.sh
@@ -11,6 +11,17 @@ if [[ $? != 0 || "${tmux_version}" != "tmux 1.8" ]]; then
     cd ..
     rm -r tmux-1.8
 fi
+
+watchman_version=$(watchman --version)
+if [[ $? != 0 ]]; then
+    cd watchman
+    ./autogen.sh && ./configure && make
+    sudo make install
+    make clean
+    rm -rf .deps/ Makefile Makefile.in aclocal.m4 autom4te.cache/ cmds/.deps/ cmds/.dirstamp compile config.guess config.h config.h.in config.log config.status config.sub configure depcomp files install-sh missing query/.deps/ query/.dirstamp stamp-h1 tests/.deps/ tests/.dirstamp thirdparty/.deps/ thirdparty/.dirstamp thirdparty/jansson/.deps/ thirdparty/jansson/.dirstamp thirdparty/jansson/jansson_config.h watcher/.deps/ watcher/.dirstamp
+    cd ..
+fi
+
 cp -R -i .astylerc .bashrc bin .gdbinit .gitconfig hg-prompt .hgrc hgwatchman .rviz .vim .vimrc ~
 cd ~
 mkdir -p workspace
