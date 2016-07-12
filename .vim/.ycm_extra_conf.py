04d52cbe9799710e2c9ce1238e23b1781650af1e
Jake Askeland <jake.askeland@gmail.com>
2016-07-12 12:05:04 -0700

    improve ycm conf

--- .vim/.ycm_extra_conf.py
+++ .vim/.ycm_extra_conf.py
@@ -112,30 +112,23 @@ def IsHeaderFile(filename):
     return extension in ['.hpp', '.hxx', '.hh', '.h', '.inl', '.impl']
 
 
+def getRosMessageFlags():
+    try:
+        import rosmsg
+    except:
+        return None
+
+    msg_list = rosmsg.list_msgs
+    flags = '-I '.join(dir + '/include'
+                       for pkg, dir in msg_list.iterate_packages)
+    return flags
+
+
 def FlagsForFile(filename, **kwargs):
-    if database:
-        # Bear in mind that compilation_info.compiler_flags_ does NOT return a
-        # python list, but a "list-like" StringVec object
-        compilation_info = GetCompilationInfoForFile(filename)
-        if not compilation_info:
-            return None
-
-        final_flags = MakeRelativePathsInFlagsAbsolute(
-            compilation_info.compiler_flags_,
-            compilation_info.compiler_working_dir_)
-
-        # NOTE: This is just for YouCompleteMe; it's highly likely that your
-        # project does NOT need to remove the stdlib flag. DO NOT USE THIS IN
-        # YOUR ycm_extra_conf IF YOU'RE NOT 100% SURE YOU NEED IT.
-        # try:
-        #     final_flags.remove('-stdlib=libc++')
-        # except ValueError:
-        #     pass
-    else:
-        flags = getDefaultFlags() + getSystemIncludeFlags() + \
-                        getLocalIncludeFlags(filename)
-        relative_to = DirectoryOfThisScript()
-        final_flags = MakeRelativePathsInFlagsAbsolute(flags, relative_to)
+    flags = getDefaultFlags() + getSystemIncludeFlags() + \
+            getLocalIncludeFlags(filename) + getRosMessageFlags()
+    relative_to = DirectoryOfThisScript()
+    final_flags = MakeRelativePathsInFlagsAbsolute(flags, relative_to)
 
     return {
         'flags': final_flags,
