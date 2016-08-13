4f860605f99f8413ff851685bccc2171812c6c13
Jake Askeland <jake.askeland@gmail.com>
2016-08-12 20:55:17 -0700

    add github to heroku

--- heroku-setup.sh
+++ heroku-setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+cd ~/Downloads
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku login
 mkdir -p ~/workspace/quirq
 cd ~/workspace/quirq
 heroku git:clone -a hidden-anchorage-47947
+git remote add --master master github git@github.com:jakalope/quirq-backend.git
