00f43f36dcc9f00642d9d0ae7de127aa470ab0e2
Jake Askeland <jake.askeland@gmail.com>
2017-05-21 23:04:07 -0700

    update setup

--- setup_neovim.sh
+++ setup_neovim.sh
@@ -8,14 +8,15 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     brew install neovim
     sudo -H pip2 install neovim
 else
-    cd ~/Downloads
-    if [[ ! -e v0.1.6.zip ]]; then
-        wget https://github.com/neovim/neovim/archive/v0.1.6.zip
-    fi
-    unzip v0.1.6.zip
-    cd neovim-0.1.6
-    make -j CMAKE_BUILD_TYPE=Release
-    sudo make install
+#    cd ~/Downloads
+#    if [[ ! -e v0.1.6.zip ]]; then
+#        wget https://github.com/neovim/neovim/archive/v0.1.6.zip
+#    fi
+#    unzip v0.1.6.zip
+#    cd neovim-0.1.6
+#    make -j CMAKE_BUILD_TYPE=Release
+#    sudo make install
+    sudo apt-get install neovim
 fi
 
 sudo gem install neovim
