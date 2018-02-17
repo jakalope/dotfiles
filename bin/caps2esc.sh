b758654d8bb6870e86d73f565303af2c1533aade
Jake Askeland <jake.askeland@gmail.com>
2018-02-16 17:09:24 -0800

    fix caps2esc.sh

--- bin/caps2esc.sh
+++ bin/caps2esc.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-sudo DEVNODE=0 /usr/bin/nice -n -20 /usr/local/bin/udevmon -c ~/dotfiles/udevmon.yaml &K
+sudo ls >/dev/null
+sudo DEVNODE=0 /usr/bin/nice -n -20 /usr/local/bin/udevmon -c ~/dotfiles/udevmon.yaml &
